e9d31605d2177837b2a91a51d5d31a9b49f1d8ac
Azadeh Hajian <Azadehajian1994@gmail.com>
2025-12-25 22:11:00 +0100

    add langsmith for tracking, openai api, api check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-
+from langchain_openai import ChatOpenAI
 
 def check_api_key():
     load_dotenv()
@@ -13,8 +13,32 @@ def check_api_key():
         print(f"Error: {e}")
         return None
 
+#Instantiation
+def llm_instance():
 
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0,
+        # max_tokens=None,
+        timeout=300,
+    )
+    return llm
 
 if __name__ == "__main__":
+    api_key = check_api_key()
+    if api_key:
+        print("API Key successfully loaded.")
+        llm = llm_instance()
+        print("LLM instance created.")
 
-
+        #Invocation
+        messages = [
+            (
+                "system",
+                "You are a helpful assistant,answer and guide the user.",
+            ),
+            ("human", "who is the president of the united states?"),
+        ]
+        ai_msg = llm.invoke(messages)
+        ai_msg
+        print(ai_msg.text)
